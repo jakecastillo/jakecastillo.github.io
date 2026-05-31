ec48f00abe195963f3e6dc53b7de112b3860411c
Jake Castillo <JCastillo+piz@PacImpactZone.com>
2026-05-31 13:25:55 -1000

    feat(apps): add Terminal app wrapper

---
+++ components/desktop/apps/TerminalApp.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import TerminalTyping from "@/components/TerminalTyping";
+
+export default function TerminalApp() {
+    return (
+        <div className="w-full h-full bg-[#1c1c1c]">
+            <TerminalTyping />
+        </div>
+    );
+}
